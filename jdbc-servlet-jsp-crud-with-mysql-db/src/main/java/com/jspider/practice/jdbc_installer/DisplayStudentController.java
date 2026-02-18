6c74d2fc17e2b5cd428c3db8feef18a36c80c0de
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-18 19:16:29 +0530

    update tha advance folder

--- jdbc-servlet-jsp-crud-with-mysql-db/src/main/java/com/jspider/practice/jdbc_installer/DisplayStudentController.java
+++ jdbc-servlet-jsp-crud-with-mysql-db/src/main/java/com/jspider/practice/jdbc_installer/DisplayStudentController.java
@@ -1,20 +1,25 @@
-package com.qspider.jdbc_servlet_jsp_crud_with_mysql_db.controller;
+package com.jspider.practice.jdbc_installer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 
+import com.mysql.cj.jdbc.Driver;
 
-public class InsertStudentController {
+public class DisplayStudentController {
 	
 	public static void main(String[] args) {
 		Connection connection=null;
 		Statement statement=null;
 		try {
 			
-			//step 1 load the driver
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			//step 1 register the driver
+    Driver driver=new Driver();
+    DriverManager.registerDriver(driver);
+    
 			//step2 connection establish
 			String url="jdbc:mysql://localhost:3306/jdbc-m17";
 			String user="root";
@@ -23,18 +28,22 @@ public class InsertStudentController {
 			connection=DriverManager.getConnection(url, user, password);
 			//step 3 create statement
 			statement=connection.createStatement();
-//			String sql="delete from student where id=103";
-//			String sql="insert into student(id,name,email,phone,password,dob) values(103,'sohan','sohan@gmail.com',645463453,'sachine@123','2000-01-01')";
-//			System.out.println(connection);
-			int a=statement.executeUpdate(sql);
-			if(a>0) {
-				System.out.println("record inserted successfully");
-			}
-			else {
-				System.out.println("record not inserted");
-			}
+			String sql="select * from student";
+			ResultSet res=statement.executeQuery(sql);
 			
-		} catch (ClassNotFoundException | SQLException e) {
+			while(res.next()) {
+				System.out.println("ID is ==> "+res.getInt("id"));
+				System.out.println("Name is  ==>"+res.getString("name"));
+//				System.out.println("Date ==>"+res.getDate("dob"));
+				
+			LocalDate dob=	res.getDate("dob").toLocalDate();
+			System.out.println(dob);
+				//fetch local date from using date
+				System.out.println(LocalDate.now());
+			}
+		
+			
+		} catch ( SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally{
@@ -42,7 +51,6 @@ public class InsertStudentController {
 				try {
 					statement.close();
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -50,12 +58,11 @@ public class InsertStudentController {
 				try {
 					connection.close();
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
+
 					e.printStackTrace();
 				}
 			}
 		}
 		
 	}
-
 }
--- jdbc-servlet-jsp-crud-with-mysql-db/src/main/java/com/jspider/practice/jdbc_installer/practiceaController.java
+++ jdbc-servlet-jsp-crud-with-mysql-db/src/main/java/com/jspider/practice/jdbc_installer/practiceaController.java
@@ -1,6 +1,11 @@
 package com.jspider.practice.jdbc_installer;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class practiceaController {
 	
@@ -16,18 +21,30 @@ public class practiceaController {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			Connection con=DriverManager.getConnection(url, user, password);
-//			System.out.println(con);
 			Statement stmt=con.createStatement();
-			ResultSet rs=stmt.executeQuery("select * from demo");
-			while(rs.next()) {
-				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
-			}
-		} catch (ClassNotFoundException | SQLException e) {
+//			String sql="insert into demo(id,name,phone) values(104,'mohit',3252520)";
+			//String sql="delete from demo where id=104";
+			String sql="select * from demo";
+			ResultSet res=stmt.executeQuery(sql);
 			
+			while(res.next()) {
+				System.out.println(res.getInt(1));
+				System.out.println(res.getString(2));
+				System.out.println(res.getString(3));
+			}
+			
+//			if(a>0) {
+//				System.out.println("data inserted");
+//			}
+//			else {
+//				System.out.println("something went wrong");
+//			}
+			
+		} catch (ClassNotFoundException| SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+	
 		
 		
 	}
